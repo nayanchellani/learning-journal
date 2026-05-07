bfb1701c0eb2f722bfc6a24f95757a441025f8ac
Nayan Chellani <chellaninayan@gmail.com>
2026-05-07 22:22:17 +0530

    Update chat.py

--- Langchain/Prompts/chat.py
+++ Langchain/Prompts/chat.py
@@ -1,14 +1,20 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage , HumanMessage , AIMessage
 
 load_dotenv()
 model= ChatGoogleGenerativeAI(model='models/gemini-2.5-flash')
-chat_history=[]
+chat_history= [
+    SystemMessage(content='you are a helpful AI assistant who provides insightful answers')
+]
+
 while True:
         user_input=input("User: ")
-        chat_history.append(user_input)
         if user_input == 'exit':
             break
+        chat_history.append(HumanMessage(content=user_input))
         result= model.invoke(chat_history)
-        chat_history.append(result.content)
+        chat_history.append(AIMessage(content=result.content))
         print('AI: ', result.content)
+        
+print('Chat history: \n',chat_history)
